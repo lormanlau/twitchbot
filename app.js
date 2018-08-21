bd6cdd7a4076ae98799372b530ac53079c2d2091
Lorman Lau <lormanlau@gmail.com>
2018-08-20 23:03:51 -0700

    added discord support

--- app.js
+++ app.js
@@ -1,5 +1,60 @@
 const tmi = require('tmi.js');
 const oAuth = require('./config.js')
+const Discord = require('discord.io')
+
+const bot = new Discord.Client({
+   token: oAuth.token,
+   autorun: true
+});
+
+bot.on('ready', function (evt) {
+    console.log('Connected');
+    console.log('Logged in as: ');
+    console.log(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+                break;
+            case "giveaway":
+            addGiveaway(options.channels[0], args[0]);
+                break;
+            case "cleargiveaway":
+                clearGiveaway(options.channels[0]);
+                break;
+            case "pickwinner":
+                pickWinner(options.channels[0]);
+                break;
+            case "message":
+                setMessages(options.channels[0], message.substring(9, message.length));
+                break;
+            case "clearmessage":
+                clearMessage(options.channels[0], message);
+                break;
+            case "updatedc":
+                updateDeathCount(options.channels[0], message);
+                break;
+            case "delete":
+                say(channel, "/timeout " + array[1] + " 1");
+                break;
+            default:
+                break;
+            }
+     }
+});
+
 
 const options = {
     options: {
